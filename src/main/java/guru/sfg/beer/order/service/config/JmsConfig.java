ca3407f743646dd00c1b7106d977b601b038820c
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2019-12-28 06:18:05 -0500

    Lecture 189 completed

---
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -0,0 +1,31 @@
+package guru.sfg.beer.order.service.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig
+{
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+    public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
+    public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
+    public static final String JMS_MSG_CONVERTER_TYPE_ID_PROPERTY_NAME = "_type";
+
+    // ObjectMapper is Autowired here
+    @Bean
+    public MessageConverter messageConverter(ObjectMapper objectMapper)
+    {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName(JMS_MSG_CONVERTER_TYPE_ID_PROPERTY_NAME);
+        converter.setObjectMapper(objectMapper);
+        return converter;
+    }
+
+
+}
